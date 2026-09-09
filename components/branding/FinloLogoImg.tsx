d9bc7db4dc86b5119aefe85de146ddf909422225
kdmhae-cloud <kd.mhae@gmail.com>
2026-09-09 21:25:26 +0500

    Auth pages: Finlo icon -> finlo-brand-mark.svg

--- components/branding/FinloLogoImg.tsx
+++ components/branding/FinloLogoImg.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function FinloLogoImg({ size = 38, radius = 10 }: { size?: number; radius?: number }) {
   return (
     <Image
-      src="/logo.png"
+      src="/finlo-brand-mark.svg"
       alt="Finlo"
       width={size}
       height={size}
